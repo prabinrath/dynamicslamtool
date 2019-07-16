41b1b7c358256c2288a239e4ad7454c0269fca22
Prabin Rath <prabinrath123@gmail.com>
2019-07-15 21:13:26 -0600

    ground plane removal modified

--- src/getpc.cpp
+++ src/getpc.cpp
@@ -1,22 +1,65 @@
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <visualization_msgs/Marker.h>
 #include <pcl/ModelCoefficients.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/octree/octree_pointcloud_changedetector.h>
+#include <pcl/filters/voxel_grid_covariance.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/common/centroid.h>
+#include <pcl/common/common.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <iostream>
 #include <unistd.h>
 #include <ctime>
+#include <string>
 
-ros::Publisher pub;
+ros::Publisher pub,marker_pub;
 pcl::PointCloud<pcl::PointXYZI>::Ptr ca, cb;
+
+visualization_msgs::Marker mark_direction(pcl::PointXYZI pt,double x,double y,double z,int id, std::string f_id, std::string ns="direction_vector", float r=0.5, float g=0.5, float b=0.5)
+{
+  uint32_t shape = visualization_msgs::Marker::LINE_STRIP;
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = f_id;
+  marker.header.stamp = ros::Time::now();
+
+  marker.ns = ns;
+  marker.id = id;
+  marker.type = shape;
+  marker.action = visualization_msgs::Marker::ADD;
+
+  marker.pose.orientation.x = 1;
+  marker.pose.orientation.y = 1;
+  marker.pose.orientation.z = 1;
+  marker.pose.orientation.w = 1;
+
+  geometry_msgs::Point p;
+  p.z = pt.x;
+  p.x = pt.y;
+  p.y = pt.z;
+  marker.points.push_back(p);
+  p.z = pt.x+x;
+  p.x = pt.y+y;
+  p.y = pt.z+z;
+  marker.points.push_back(p);
+
+  marker.scale.x = 0.04;
+  
+  marker.color.r = r;
+  marker.color.g = g;
+  marker.color.b = b;
+  marker.color.a = 0.8;
+
+  marker.lifetime = ros::Duration(1);
+  return marker;
+}
 
 void detect_change(const sensor_msgs::PointCloud2ConstPtr& input)
 {
@@ -104,34 +147,59 @@ void statistical_outlier_removal(const sensor_msgs::PointCloud2ConstPtr& input)
     pub.publish(output);
 }
 
-void pass_through(const sensor_msgs::PointCloud2ConstPtr &input)
+void groundPlaneRemoval(const sensor_msgs::PointCloud2ConstPtr &input)
 {
     sensor_msgs::PointCloud2 output;
     pcl::PCLPointCloud2 *cloud = new pcl::PCLPointCloud2;
     pcl_conversions::toPCL(*input, *cloud);
     pcl::fromPCLPointCloud2(*cloud, *ca);
-    
-    pcl::PassThrough<pcl::PointXYZI> pass;
-    pass.setInputCloud(ca);
-    pass.setFilterFieldName("z");
-    pass.setFilterLimits(-0.5, 5.0);
-    pass.filter(*cb);
+    cb.reset(new pcl::PointCloud<pcl::PointXYZI>);
 
-    *ca = *cb;
-    pass.setInputCloud(ca);
-    pass.setFilterFieldName("x");
-    pass.setFilterLimits(-3.0, 3.0);
-    pass.filter(*cb);
+    pcl::PointCloud<pcl::PointXYZI> dsc;
+    pcl::VoxelGrid<pcl::PointXYZI> vg;
+    vg.setInputCloud(ca);
+    vg.setLeafSize(0.1,0.1,0.1);
+    vg.filter(dsc);
 
-    *ca = *cb;
-    pass.setInputCloud(ca);
-    pass.setFilterFieldName("y");
-    pass.setFilterLimits(-3.0, 3.0);
-    pass.filter(*cb);
-    
+    pcl::KdTreeFLANN<pcl::PointXYZI>::Ptr tree(new pcl::KdTreeFLANN<pcl::PointXYZI>);
+    tree->setInputCloud(ca);
+    Eigen::Vector4f min,max;
+    pcl::getMinMax3D(dsc, min, max);
+
+    for(int i=0;i<dsc.points.size();i++)
+    {
+      std::vector<int> ind;
+      std::vector<float> dist;
+
+      if(tree->radiusSearch(dsc.points[i], 0.1, ind, dist) > 0 )
+      //if(tree->nearestKSearch(dsc.points[i], 20, ind, dist) > 0 )
+      {
+        if(ind.size()>10)
+        {
+          pcl::PointCloud<pcl::PointXYZI> temp;
+          for(int j=0;j<ind.size();j++)
+            temp.points.push_back(ca->points[ind[j]]);
+          temp.width = temp.points.size();
+          temp.height = 1;
+
+          Eigen::Vector4f cp;
+          pcl::compute3DCentroid(temp, cp);
+          Eigen::Matrix3f covariance_matrix;
+          pcl::computeCovarianceMatrix(temp, cp, covariance_matrix);
+          if(fabs(covariance_matrix(0,2))<0.001 && fabs(covariance_matrix(1,2))<0.001 && fabs(covariance_matrix(2,2))<0.001 && fabs(min[2]-dsc.points[i].z)<0.1)
+          {
+            std::cout<<covariance_matrix<<std::endl<<std::endl;
+            cb->points.push_back(dsc.points[i]);
+          }
+        }
+      }
+    }
+
+    cb->width = cb->points.size();
+    cb->height = 1;
     pcl::toPCLPointCloud2(*cb,*cloud);
     pcl_conversions::fromPCL(*cloud, output);
-    output.header.frame_id = "/test";
+    output.header.frame_id = "/current";
     pub.publish(output);
 }
 
@@ -146,7 +214,7 @@ void ransac_check(const sensor_msgs::PointCloud2ConstPtr &input)
     pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
 
     pcl::SACSegmentation<pcl::PointXYZI> seg;
-    seg.setOptimizeCoefficients(true);
+    //seg.setOptimizeCoefficients(false);
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
     seg.setDistanceThreshold(0.1);
@@ -172,7 +240,7 @@ void ransac_check(const sensor_msgs::PointCloud2ConstPtr &input)
   
   pcl::toPCLPointCloud2(*cb,*cloud);
   pcl_conversions::fromPCL(*cloud, output);
-  output.header.frame_id = "/test";
+  output.header.frame_id = "/current";
   pub.publish(output);
 }
 
@@ -180,10 +248,13 @@ int main (int argc, char** argv)
 {
   ros::init (argc, argv, "test_pcl");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe ("/velodyne_points", 1, statistical_outlier_removal);
+  ros::Subscriber sub = nh.subscribe ("/velodyne_points", 1, groundPlaneRemoval);
   pub = nh.advertise<sensor_msgs::PointCloud2> ("output", 1);
+  marker_pub = nh.advertise<visualization_msgs::Marker>("bbox", 10);
+
   ca.reset(new pcl::PointCloud<pcl::PointXYZI>);
   cb.reset(new pcl::PointCloud<pcl::PointXYZI>);
   ca->height = -1; cb->height = -1;
+
   ros::spin();
 }
